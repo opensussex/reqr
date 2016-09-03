ee547128b62e70d4b007015e569b2355b5267ab2
opensussex <opensussex@gmail.com>
2016-09-03 14:11:02 +0100

    get the json pretty printed

--- reqr.go
+++ reqr.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -14,9 +16,19 @@ func req(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Form["url_long"])
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
+		fmt.Println("Ooops! somethings gone wrong")
 		panic(err)
 	}
-	fmt.Println(string(body))
+
+	var prettyJSON bytes.Buffer
+	err = json.Indent(&prettyJSON, body, "", "\t")
+	if err != nil {
+		fmt.Println("Ooops! somethings gone wrong")
+		panic(err)
+	}
+	fmt.Println("Body: ")
+	fmt.Println(string(prettyJSON.Bytes()))
+	fmt.Println("Params: ")
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
@@ -25,6 +37,7 @@ func req(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	fmt.Println("Send Requests to: http://localhost:8666")
+	fmt.Println("Ctrl-C exit!")
 	http.HandleFunc("/", req)
 	err := http.ListenAndServe(":8666", nil)
 	if err != nil {
